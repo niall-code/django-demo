9f8887e2fd2d5993f2c0e6f10882deaba9d32f8b
Niall Caplis <niallcaplis@gmail.com>
2024-07-07 16:21:41 +0000

    Alter url pattern for hello_world's views.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from hello_world import views as hello_world_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('hello/', hello_world_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
